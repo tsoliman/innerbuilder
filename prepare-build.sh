bc0d01c642e13e0e130f9589f055aa4bf9e7cc68
Tarek Soliman <tarek@bashasoliman.com>
2019-05-07 12:03:52 -0500

    update prepare-build.sh to a working link

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p target/dependency/intellij-idea
-curl -L https://download.jetbrains.com/idea/ideaIC-2017.2.5.tar.gz | tar xz --strip-components=1 -C target/dependency/intellij-idea
+curl -L https://download-cf.jetbrains.com/idea/ideaIC-2019.1.1.tar.gz | tar xz --strip-components=1 -C target/dependency/intellij-idea
